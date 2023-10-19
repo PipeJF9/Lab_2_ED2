a3350dc8cd1660305096e69bde4ffd176eada671
Pipe Santos <jufesaro874@gmail.com>
2023-10-19 18:18:34 -0500

    falta la creación de aristas

--- src/Map.py
+++ src/Map.py
@@ -3,16 +3,25 @@ from folium.plugins import MarkerCluster
 m = folium.Map()
 
 marker_cluster = MarkerCluster().add_to(m)
-
+folium.plugins.LocateControl(auto_start=True).add_to(m)
 
 def add_markers(graph):
     for dic in graph.nodes:
-        __add_marker(dic.Source_Airport_Latitude, dic.Source_Airport_Longitude, dic.Source_Airport_Name, dic.Source_Airport_Code, dic.Source_Airport_City, dic.Source_Airport_Country)
+        __add_marker(dic.Source_Airport_Latitude, dic.Source_Airport_Longitude, dic.Source_Airport_Name, dic)
     return m
 
 
-def __add_marker(lat, lon, name, code, city, country):
-    folium.Marker(location=[lat, lon], popup=f"{name}\n{code}\n{city}\n{country}").add_to(marker_cluster)
+def __add_marker(lat, lon, name, dic):
+    html = f"""
+          <b>Airport:</b></br>
+          <code>{dic.Source_Airport_Code}</code>
+          <p>{name}</p>
+          <p>{dic.Source_Airport_City}</p>
+          <p>{dic.Source_Airport_Country}</p>
+          <code>{dic.Source_Airport_Latitude}</code>
+          <code>{dic.Source_Airport_Longitude}</code>
+        """
+    folium.Marker(location=[lat, lon], popup=html, icon=folium.Icon(color='blue', icon='plane', prefix='fa'), tooltip="Click me!").add_to(marker_cluster)
 
 
 def aristas(lat1, lon1, lat2, lon2, code1, code2):
